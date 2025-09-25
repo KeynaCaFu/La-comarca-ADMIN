bc8b5e70d5f290f80c0e24af3da67c7654f37c9b
RECV <134547520+RECV00@users.noreply.github.com>
2025-09-24 21:45:21 -0600

    cambios

--- storage/framework/views/78a4ac5e85ef94207cef10e7ec7975c82f5e995f.php
+++ storage/framework/views/78a4ac5e85ef94207cef10e7ec7975c82f5e995f.php
@@ -1,4 +1,4 @@
-extends('layouts.app')
+
 
 <?php $__env->startSection('title', 'Detalles del Insumo'); ?>
 
@@ -112,4 +112,5 @@ extends('layouts.app')
         </div>
     </div>
 </div>
-<?php $__env->stopSection(); ?><?php /**PATH C:\xampp\htdocs\La-comarca-ADMIN\resources\views/insumos/show.blade.php ENDPATH**/ ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\La-comarca-ADMIN\resources\views/insumos/show.blade.php ENDPATH**/ ?>
