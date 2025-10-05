accd7eead2362019278c94b13384f9f3648df9bc
KeynaCaFu <keyna.castro.fuentes@est.una.ac.cr>
2025-10-05 14:24:10 -0600

    se agrego validaciones a insumo
    valida, fecha vencimiento y stock bajo , ademas formulario

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,47 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InsumoController;
 use App\Http\Controllers\ProveedorController;
-use Illuminate\Support\Facades\Route;
 
-// Página de bienvenida inicial
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// Ruta de bienvenida
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-// Dashboard principal del sistema
+// Ruta del dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
 // Rutas de Insumos
-Route::resource('insumos', InsumoController::class);
-Route::get('insumos/{id}/show-modal', [InsumoController::class, 'showModal'])->name('insumos.show-modal');
-Route::get('insumos/{id}/edit-modal', [InsumoController::class, 'editModal'])->name('insumos.edit-modal');
+Route::prefix('insumos')->name('insumos.')->group(function () {
+    Route::get('/', [InsumoController::class, 'index'])->name('index');
+    Route::post('/', [InsumoController::class, 'store'])->name('store');
+    Route::get('/{id}', [InsumoController::class, 'show'])->name('show');
+    Route::put('/{id}', [InsumoController::class, 'update'])->name('update');
+    Route::delete('/{id}', [InsumoController::class, 'destroy'])->name('destroy');
+    
+    // Rutas para modales
+    Route::get('/{id}/show-modal', [InsumoController::class, 'showModal'])->name('show.modal');
+    Route::get('/{id}/edit-modal', [InsumoController::class, 'editModal'])->name('edit.modal');
+});
 
-// Rutas de Proveedores (corregidas)
-Route::resource('proveedor', ProveedorController::class);
-Route::get('proveedor/{id}/show-modal', [ProveedorController::class, 'showModal'])->name('proveedor.show-modal');
-Route::get('proveedor/{id}/edit-modal', [ProveedorController::class, 'editModal'])->name('proveedor.edit-modal');
+// Rutas de Proveedores
+Route::prefix('proveedor')->name('proveedor.')->group(function () {
+    Route::get('/', [ProveedorController::class, 'index'])->name('index');
+    Route::post('/', [ProveedorController::class, 'store'])->name('store');
+    Route::get('/{id}', [ProveedorController::class, 'show'])->name('show');
+    Route::put('/{id}', [ProveedorController::class, 'update'])->name('update');
+    Route::delete('/{id}', [ProveedorController::class, 'destroy'])->name('destroy');
+    
+    // Rutas para modales
+    Route::get('/{id}/show-modal', [ProveedorController::class, 'showModal'])->name('show.modal');
+    Route::get('/{id}/edit-modal', [ProveedorController::class, 'editModal'])->name('edit.modal');
+});
